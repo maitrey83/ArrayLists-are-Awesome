eccf6fae2798d1a7024f87515b44e5aaa7f03289
S <s>
2013-02-13 14:56:44 -0800

    {"type":"TI", "runs":"0"}

--- NotSoSimpleArrayLists.java
+++ NotSoSimpleArrayLists.java
@@ -9,19 +9,6 @@ import java.util.*;
 
 public class NotSoSimpleArrayLists extends ConsoleProgram {
 	public void run() {
-		setFont("DejaVuSerif-BOLD-24");
-		
-		ArrayList<Integer> lines = new ArrayList<Integer>();
-		
-		while (true) {
-			int value = readInt("Enter a value: ");
-			if (value == 0) break;
-			
-			lines.add(value);
-		}
-		
-		for (int i = 0; i < lines.size(); i++) {
-			println("Line #" + i + ": " + lines.get(i));
-		}
+
 	}
 }
