7481a8cd6db25e8cd0c21e83f8ad8b82d79faf7e
S <s>
2013-02-13 15:58:04 -0800

    {"type":"TI", "runs":"1"}

--- SeeTheUS.java
+++ SeeTheUS.java
@@ -65,6 +65,7 @@ public class SeeTheUS extends GraphicsProgram {
 	}
 	
 	private void visualizeTheUS(ArrayList<City> cities) {
+		getGCanvas().setAutoRepaintFlag(false);
 		for (int x = 0; x < getWidth(); x++) {
 			for (int y = 0; y < getHeight(); y++) {
 				double longitude = xCoordinateToLongitude(x);
