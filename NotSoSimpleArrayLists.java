85204bd36e09f8e45a6b4b4d83dab4e2bd2dc6dc
S <s>
2013-02-13 15:30:30 -0800

    {"type":"TI", "runs":"2"}

--- NotSoSimpleArrayLists.java
+++ NotSoSimpleArrayLists.java
@@ -9,6 +9,20 @@ import java.util.*;
 
 public class NotSoSimpleArrayLists extends ConsoleProgram {
 	public void run() {
-
+		/* ArrayLists are too awesome to have small fonts! */
+		setFont("DejaVuSerif-BOLD-24");
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		while (true) {
+			String line = readLine("Enter a string: ");
+			if (line.equals("")) break;
+			
+			list.add(line);
+		}
+		
+		for (int i = 0; i < list.size(); i++) {
+			println("Line #" + i + " is " + list.get(i));
+		}
 	}
 }
