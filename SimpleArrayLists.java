5d098a27f979282da46e105c26027098db61886c
S <s>
2013-02-13 22:37:58 -0800

    {"type":"TI", "runs":"0"}

--- SimpleArrayLists.java
+++ SimpleArrayLists.java
@@ -6,8 +6,10 @@ public class SimpleArrayLists extends ConsoleProgram {
 		/* ArrayLists are too awesome to have small fonts! */
 		setFont("DejaVuSerif-BOLD-24");
 		
+		/* Create a list of all input we'll read in. */
 		ArrayList<String> list = new ArrayList<String>();
 		
+		/* Read data until we get the empty string. */
 		while (true) {
 			String line = readLine("Enter a string: ");
 			if (line.equals("")) break;
@@ -15,6 +17,7 @@ public class SimpleArrayLists extends ConsoleProgram {
 			list.add(line);
 		}
 		
+		/* Output all the strings we read. */ 
 		for (int i = 0; i < list.size(); i++) {
 			println("Line #" + i + " is " + list.get(i));
 		}
